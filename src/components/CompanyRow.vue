120a50296ae760dbc4349f1f71ce4f5f51e9c2e8
Renato Vicente <revi66@hotmail.com>
2019-08-28 12:53:27 -0300

    loading companies

---
+++ src/components/CompanyRow.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout>
+    <v-flex xs12>
+      {{ item.name }}
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,112 @@
 <template>
-  <div></div>
+  <v-fade-transition>
+    <v-container fill-height v-if="loading">
+      <v-layout align-center>
+        <v-flex class="text-center">
+          <v-progress-circular></v-progress-circular>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-container v-else>
+      <CompanyRow :item="company" v-for="(company, index) in companies" :key="index"/>
+    </v-container>
+  </v-fade-transition>
 </template>
 
 <script>
-
+import CompanyRow from '@/components/CompanyRow'
 export default {
-  // components: {
-  // }
+  components: {
+    CompanyRow
+  },
+  data () {
+    return {
+      loading: true,
+      companies: []
+    }
+  },
+  methods: {
+    async loadCompanies () {
+      this.loading = true
+      this.companies = await this.getCompanies()
+      this.loading = false
+    },
+    async getCompanies () {
+      let md = await this.getMdfile()
+      let companies = []
+      let open = false
+      md.split('\n').forEach(line => {
+        if (line[0] === '[') {
+          companies.push({
+            name: line.split(']')[0].split('[')[1],
+            url: line.split('(')[1].split(')')[0]
+          })
+          open = true
+          return
+        }
+        if (companies.length > 0) {
+          if (companies[companies.length - 1].name === 'DB1 Global Software') {
+            console.log(line)
+          }
+        }
+        if (open) {
+          companies[companies.length - 1].tech = []
+          line.split(',').forEach(elem => {
+            companies[companies.length - 1].tech.push(elem.trim())
+          })
+          open = false
+          return
+        }
+        if (line[0] === '_') {
+          const places = line.replace('Remoto', '')
+          if (line.includes('Remoto')) {
+            companies[companies.length - 1].remote = true
+          } else {
+            companies[companies.length - 1].remote = false
+          }
+          if (places.length === 2) {
+            return
+          }
+          const manyPlaces = places.split('|')
+          const pushAddress = (location) => {
+            if (companies[companies.length - 1].address === undefined) {
+              companies[companies.length - 1].address = []
+            }
+            try {
+              companies[companies.length - 1].address.push({
+                city: location.split('/')[0].split('_')[1],
+                state: location.split('/')[1].split('_')[0]
+              })
+            } catch (err) {
+              console.log(err, line, companies[companies.length - 1].name)
+            }
+          }
+          manyPlaces.forEach(location => {
+            pushAddress(location)
+          })
+        }
+      })
+      return companies
+    },
+    getMdfile () {
+      return new Promise(async (resolve, reject) => {
+        const result = await fetch('https://raw.githubusercontent.com/Renato66/empresas-que-usam-vue-no-brasil/master/README.md').then(result => result.blob())
+        // const result = await fetch('https://raw.githubusercontent.com/vuejs-br/empresas-que-usam-vue-no-brasil/master/README.md').then(result => result.blob())
+        const reader = new FileReader()
+
+        // This fires after the blob has been read/loaded.
+        reader.addEventListener('loadend', (e) => {
+          const text = e.srcElement.result
+          resolve(text)
+        })
+
+        // Start reading the blob as text.
+        reader.readAsText(result)
+      })
+    }
+  },
+  mounted () {
+    this.loadCompanies()
+  }
 }
 </script>
