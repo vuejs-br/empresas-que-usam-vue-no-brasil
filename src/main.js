c5a3b9da8895731dca5e4b73f4365982b327ba3c
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-01-20 01:34:35 -0300

    ⚡️ delay vue startup

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
+import './assets/scss/main.scss'
+
 import Vue from 'vue'
 import Root from './Root.vue'
 import useIdle from './lib/on-idle/plugin'
+import { onIdle } from './lib/on-idle'
 
 import './registerServiceWorker'
-
-import './assets/scss/main.scss'
 
 Vue.use(useIdle)
 
 Vue.config.productionTip = process.env !== 'production'
 
-new Vue({
-  created () {
-    document
-      .body.classList.remove('starting')
-  },
-  render: h => h(Root)
-}).$mount('#app')
+onIdle(() => {
+  new Vue({
+    created () {
+      document
+        .body.classList.remove('starting')
+    },
+    render: h => h(Root)
+  }).$mount('#app')
+})
